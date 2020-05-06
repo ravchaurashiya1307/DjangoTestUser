5492c16a6b3f077d05343fad3b1dab982999406d
Ravi Shekhar <59179945+ravchaurashiya1307@users.noreply.github.com>
2020-05-06 15:32:37 +0530

    Add files via upload

--- usertest/settings.py
+++ usertest/settings.py
@@ -23,14 +23,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y-gs-ru%!o9p_wom98(o7(ezcr+z1%&6+tc=og)_y*(ezc7o^t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['myuserproject.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'storages',
     'userform',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -114,10 +115,17 @@ USE_L10N = True
 
 USE_TZ = True
 
+AWS_ACCESS_KEY_ID = 'AKIA2SF4TI6OTA5A2CCY'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'mynewuserproject'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
